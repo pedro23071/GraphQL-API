f35f57ae1fe3d910a9c7a59c97fb294f39c1e555
pedro23071 <pedro23071@gmail>
2020-03-22 18:44:37 -0600

    update the project

---
+++ app/Model/Addres.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Addres extends Model
+{
+    protected $table = "addresses";
+    public $timestamps = false;
+    protected $fillable = ['user_id','name','description'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require ('vendor/autoload.php');
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'graphqldatabase',
+    'username'  => 'root',
+    'password'  => 'panc23071',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+print_r(User::find(1))->toArray();
+var_dump("hola desde index");
